05c18ddd14add520ebe5f2bd27b026fb551ec22e
Tharanithar <rtharanithar@gmail.com>
2026-04-01 18:22:11 +0530

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const PORT = 3000;
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200);
+    res.end("Hello from the server, Tharanithar!");
+});
+
+server.listen(PORT, "0.0.0.0", () =>{
+    console.log(`SERVER is running on ${PORT}`);
+});
